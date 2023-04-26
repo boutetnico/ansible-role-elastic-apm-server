cf8959e8890c284de97d1e312ff662b6308e7a78
Nicolas Boutet <amd3002@gmail.com>
2023-04-26 15:25:04 +0200

    Adapt CI workflow for molecule 5

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -5,20 +5,27 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('name', [
-  ('apm-server'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("apm-server"),
+    ],
+)
 def test_packages_are_installed(host, name):
     package = host.package(name)
     assert package.is_installed
 
 
-@pytest.mark.parametrize('username,groupname,path', [
-  ('apm-server', 'apm-server', '/etc/apm-server/apm-server.yml'),
-])
+@pytest.mark.parametrize(
+    "username,groupname,path",
+    [
+        ("apm-server", "apm-server", "/etc/apm-server/apm-server.yml"),
+    ],
+)
 def test_config_file_exists(host, username, groupname, path):
     config = host.file(path)
     assert config.exists
@@ -27,9 +34,12 @@ def test_config_file_exists(host, username, groupname, path):
     assert config.group == groupname
 
 
-@pytest.mark.parametrize('name', [
-  ('apm-server'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("apm-server"),
+    ],
+)
 def test_service_is_running_and_enabled(host, name):
     service = host.service(name)
     assert service.is_enabled
